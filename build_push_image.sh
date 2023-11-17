5a49c6462c2c3d57ad4b09559eb661e0481fa8db
Sofyan Egi Lesmana <sofyanegil@gmail.com>
2023-11-17 15:57:03 +0700

    Change to Github Container Registry

--- build_push_image.sh
+++ build_push_image.sh
@@ -4,8 +4,8 @@ docker build -t item-app:v1 .
 # Show docker images
 docker images
 # Change tag images
-docker tag item-app:v1 sofyanegi/item-app:v1
-# Login to Docker Hub Registry
-echo $PASSWORD_DOCKER_HUB | docker login -u sofyanegi --password-stdin
-# Push images to Docker Hub Registry
-docker push sofyanegi/item-app:v1
+docker tag item-app:v1 ghcr.io/sofyanegil/item-app:v1
+# Login to GitHub Container Registry
+echo $CR_PAT | docker login ghcr.io -u sofyanegil --password-stdin
+# Push images to GitHub Container Registry
+docker push ghcr.io/sofyanegil/item-app:v1
