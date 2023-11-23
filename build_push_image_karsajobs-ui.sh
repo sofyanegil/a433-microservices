d68fdcdd5312148cf0b16829a23e92bd00e3e673
Sofyan Egi Lesmana <sofyanegil@gmail.com>
2023-11-23 21:43:19 +0700

    build and push image script

---
+++ build_push_image_karsajobs-ui.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Build and gave a tag to the image
+docker build -t ghcr.io/sofyanegil/karsajobs-ui:latest .
+# Login to GitHub Container Registry
+echo $CR_PAT | docker login ghcr.io -u sofyanegil --password-stdin
+# Push the image to Github Container Registry
+docker push ghcr.io/sofyanegil/karsajobs-ui:latest
